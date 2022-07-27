13fe2355446e38e47818065f24495dc223a6daf5
root <eme.gaby.do@gmail.com>
2022-07-27 19:34:27 +0000

    task 6...

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,21 +5,19 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
 
-  return Promise.allSettled([user, photo]).then((res) => {
-    const array = [];
-    res.forEach((elem) => {
-      if (elem.status === 'fulfilled') {
-        array.push({
-          status: elem.status,
-          value: elem.value,
-        });
-      } else {
-        array.push({
-          status: elem.status,
-          value: `${elem.reason}`,
-        });
-      }
+  return Promise.allSettled([user, photo])
+    .then((res) => {
+      const array = [];
+      res.forEach((elem) => {
+        if (elem.status === 'fulfilled') {
+          array.push(elem);
+        } else {
+          array.push({
+            status: elem.status,
+            value: `${elem.reason}`,
+          });
+        }
+      });
+      return array;
     });
-    return array;
-  });
 }
