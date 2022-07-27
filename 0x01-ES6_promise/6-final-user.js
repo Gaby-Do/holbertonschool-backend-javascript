9c80cc99eed512854317986e76e1e2f90aced851
root <eme.gaby.do@gmail.com>
2022-07-27 19:17:34 +0000

    task 6...

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo]).then((res) => {
     const array = [];
-    values.forEach((values) => {
+    res.forEach((values) => {
       if (values.status === 'fulfilled') {
-        array.push(values);
+        array.push({
+          status: values.status,
+          value: values.value,
+        });
       } else {
         array.push({
           status: values.status,
