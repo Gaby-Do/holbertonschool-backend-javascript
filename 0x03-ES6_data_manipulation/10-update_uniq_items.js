38fc9ec9a676259817db19d749ce72239bf40f64
root <eme.gaby.do@gmail.com>
2022-08-02 22:19:06 +0000

    task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(mapa) {
+  if (mapa instanceof Map) {
+    for (const [key, value] of mapa) {
+      if (value === 1) {
+        mapa.set(key, 100);
+      }
+    }
+    return mapa;
+  }
+  throw new Error('Cannot process');
+}
